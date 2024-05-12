1739e3c8035c64e5d46c7444285010b59415211d
Shefa <shefaoudeen123@gmail.com>
2024-05-12 20:00:02 +0530

    chumma

---
+++ nss-web-app/src/Data/nav_data.js
@@ -0,0 +1,9 @@
+const navbar_ele = [
+  { title: "Home", color: "red" },
+  { title: "Events", color: "yellow" },
+  { title: "About Us", color: "blue" },
+  { title: "Office Bearers", color: "orange" },
+  { title: "Contact Us", color: "green" },
+];
+
+export default navbar_ele;
--- nss-web-app/src/Navbar.jsx
+++ nss-web-app/src/Navbar.jsx
@@ -1,40 +1,50 @@
-import { useState } from 'react'
-import './input.css'
+import "./input.css";
+import navbar_ele from "./Data/nav_data";
 
 function App() {
-  const navbar_ele = [{title:'Home',color:'red-400'},{title:'Events',color:'yellow-400'},{title:'About Us',color:'blue-400'},{title:'Office Bearers',color:'orange-400'},{title:'Contact Us',color:'green-400'}]
   /* const [navbar , updatenavbar] = useState(navbar_ele) */
 
-
-
   return (
-    <div className=' font-serif'>
-      <nav className=' m-4 flex flex-row flex-wrap justify-between items-center'>
-        <div className=' ml-3 pl-8'>
-          <a className=' inline-flex' style={{textDecoration: 'none'}} href="">
-            <div className=' inline-flex'>
-              <img className=' p-2' style={{width : '90px' , height : '90px'}} src="NSS1.png" alt="" />
+    <div className=" font-serif">
+      <nav className=" m-4 flex flex-row flex-wrap justify-between items-center">
+        <div className=" ml-3 pl-8">
+          <a
+            className=" inline-flex"
+            style={{ textDecoration: "none" }}
+            href=""
+          >
+            <div className=" inline-flex">
+              <img
+                className=" p-2"
+                style={{ width: "90px", height: "90px" }}
+                src="NSS1.png"
+                alt=""
+              />
             </div>
-            <div className=' pt-6 p-3 inline-flex align-middle flex-col' >
-              <p className=' text-3xl' style={{unicodeBidi: 'isolate'} }>National Service Scheme</p>
-              <p className='text-sm ' style={{unicodeBidi: 'isolate'}}>Puducherry Technological University, Puducherry</p>
+            <div className=" pt-6 p-3 inline-flex align-middle flex-col">
+              <p className=" text-3xl" style={{ unicodeBidi: "isolate" }}>
+                National Service Scheme
+              </p>
+              <p className="text-sm " style={{ unicodeBidi: "isolate" }}>
+                Puducherry Technological University, Puducherry
+              </p>
             </div>
-
           </a>
-  
         </div>
-        <div className=' text-lg'>
-        {navbar_ele.map((ele)=>(          
-        <span className=' p-3' >
-            <button className={`p-3  rounded-xl hover:bg-${ele.color} active:bg-${ele.color}  focus:outline-none  focus:ring-2  focus:ring-${ele.color} `}style={{textDecoration: 'none', color: 'black'}} >{ele.title}</button>
-          </span>
-        ))}
+        <div className="text-lg">
+          {navbar_ele.map((ele) => (
+            <span className=" p-3">
+              <button
+                className={`p-3 rounded-xl bg-slate-300 border border-black hover:bg-${ele.color}-400`}
+              >
+                {ele.title}
+              </button>
+            </span>
+          ))}
         </div>
-
-
       </nav>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
